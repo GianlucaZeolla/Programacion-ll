fd27b590dff8fb7084647b18694fd0bcfa21b193
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-08-29 21:33:29 -0300

    Create ejercicio-3.java

---
+++ ejercicio-3.java
@@ -0,0 +1,22 @@
+package aa;
+
+public class a3 {
+	public static void main(String[] args) {
+		int a= 100; int b =200; int c=400;
+		System.out.println("el menor es " + menorDe(a,b,c));
+	}
+	private static int menorDe(int a, int b, int c) {
+		
+		int rta = a;
+		if(a < b) {
+			rta = a;
+		} else if (a > b) {
+			
+		} else if (a < c) {
+			
+		}
+		return rta;
+		
+	}
+
+}
