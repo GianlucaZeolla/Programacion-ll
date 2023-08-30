610280b0e9029542302dbbafd4a4d3d3ed850577
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-08-29 21:33:54 -0300

    Create ejercicio.1.java

---
+++ ejercicio.1.java
@@ -0,0 +1,21 @@
+package aa;
+
+public class a1 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+		
+		int suma = a+b;
+		int resta = a-b;
+		int multiplicacion = a*b;
+		int division = a/b;
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		
+	}
+}
+	
