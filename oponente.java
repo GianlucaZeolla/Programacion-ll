33fc0daa93a3c17f32567f5c3fbae38d36ea4823
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-09-04 16:43:11 -0300

    Update oponente.java

--- oponente.java
+++ oponente.java
@@ -8,26 +8,26 @@ public class Main {
 
         String oponente1 = "rojo";
         String oponente2 = "azul";
-        int barraProgresoRojo = random.nextInt(10);
-        int barraProgresoAzul = random.nextInt(10);
-        int finDelJuego = 0;
-        final boolean b = true;
+        int barraProgresoRojo = random.nextInt(10 + 1);
+        int barraProgresoAzul = random.nextInt(10 + 1);
+        String finDelJuego = null;
+    
+
+        do {
 
 
-        System.out.println("bienvenido al juego de 2 oponentes, ellos son: " + oponente1 + " y " + oponente2);
+            System.out.println("bienvenido al juego de 2 oponentes, ellos son: " + oponente1 + " y " + oponente2);
 
-        while (barraProgresoRojo < 11) {
-            System.out.println(" la barra de progreso de " + oponente1 + " es " + barraProgresoRojo);
-            System.out.println(" la barra de progreso de " + oponente2 + " es " + barraProgresoAzul);
-            if (barraProgresoRojo == finDelJuego) {
-                System.out.println("ingrese o si deseo terminar" + finDelJuego);
-            }
-            break;
+            
+                System.out.println(" la barra de progreso de " + oponente1 + " es " + barraProgresoRojo);
+                System.out.println(" la barra de progreso de " + oponente2 + " es " + barraProgresoAzul);
+                System.out.println("si desea continuar ingrese 1 en caso que no ingrese 2");
+                finDelJuego = scanner.nextLine();
+
+            } while (finDelJuego.equalsIgnoreCase("1"));
+
+            System.out.println("fin del juego");
+            scanner.close();
+
         }
-        finDelJuego++;
-
-        System.out.println("fin del juego");
-
-
-    }
-}
+        }
