bd08e001f3e3deefc92fbc35ba2c5b69acf37959
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-09-05 21:55:48 -0300

    Update tp.java

--- tp.java
+++ tp.java
@@ -7,3 +7,59 @@
 //----------------------
 //0 true, 1 true,  2 false, 3 false, 4 false, 5 false, 6 false = empate
 //0 false, 1 true, 2 false, 3 false, 4 false , 5 false, 6 false = gana jugador 2
+public class tp {
+    public static void main(String[] args) {
+        boolean[] resultados = jugar();
+        mostrar(resultados);
+    }
+
+    private static boolean[] jugar() {
+
+
+    }
+
+    private static void mostrar(boolean[] resultados) {
+        System.out.println(ganador(resultados));
+        System.out.println(asertadorTodos(resultados));
+        System.out.println(asertadorPorLoMenosUno(resultados));
+    }
+
+    private static boolean asertadorTodos(boolean[] resultados) {
+        return false;
+    }
+
+    public static boolean asertadorPorLoMenosUno(boolean[] resultados) {
+    }
+
+    private static String ganador(boolean[] resultados) {
+        int puntosJugador1 = puntosJugador1(resultados);
+        int puntosJugador2 = puntosJugador2(resultados);
+        if (puntosJugador1==puntosJugador2){
+            return "empate";
+        } else if (puntosJugador1 > puntosJugador2) {
+            return "gano jugador 1 ";
+        } else {
+            return "gano jugador 2";
+        }
+    }
+
+    public static int puntosJugador2(boolean[] resultados) {
+        int rta = 0;
+        for (int i = 1; i < resultados.length; i=i+2){
+            if (resultados[i]) rta++;
+        }
+        return rta;
+    }
+
+    private static int puntosJugador1(boolean[] resultados) {
+
+
+        return 0;
+    }
+
+    private static int jugar() {
+        return 0;
+    }
+
+}
+
