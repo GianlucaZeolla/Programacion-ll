83c4f33a02743231d90b34d6dc7b594a3e62ea53
GianlucaZeolla <134320533+GianlucaZeolla@users.noreply.github.com>
2023-09-05 21:50:47 -0300

    Create tp.java

---
+++ tp.java
@@ -0,0 +1,9 @@
+//realizar un juego de 2 jugadores, adivinando del 1 al 10 un numero al azar, en 10 jugadas.
+//condiciones: estructura de datos un Array. invariante de representacion, en las posiciones pares jugador 1, impares, jugador 2.
+//tomamos el 0 como par
+//1-indicar quien gano
+//2-indicar si se acertaron todos los puntos
+//3-indicar si se acerto algun punto
+//----------------------
+//0 true, 1 true,  2 false, 3 false, 4 false, 5 false, 6 false = empate
+//0 false, 1 true, 2 false, 3 false, 4 false , 5 false, 6 false = gana jugador 2
